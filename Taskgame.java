3c5eb9e3b9f5ae9df88fe19522bc6dddb6dd6030
Park minseok <64785907+toppms@users.noreply.github.com>
2020-05-11 16:59:51 +0900

    Update DoubleRsp game
    묵지빠 게임 룰 적용 및 구현

--- Taskgame.java
+++ Taskgame.java
@@ -1,64 +1,130 @@
 //묵찌빠 게임 코드 작성
 
-import java.util.scanner;
+import java.util.*;
 
-public class DoubleRps { 
-    
+public class DoubleRps   { 
+	
+
+	
+	static void display(int user, int com) {
+		   switch(user) {
+           case 1:
+           	System.out.println("User : 묵");
+           	break;
+           case 2:
+           	System.out.println("User : 찌");
+           	break;
+           case 3:
+           	System.out.println("User : 빠");
+           	break;
+           }
+           switch(com) {
+           case 1:
+           	System.out.println("Com : 묵");
+           	break;
+           case 2:
+           	System.out.println("Com : 찌");
+           	break;
+           case 3:
+           	System.out.println("Com : 빠");
+           	break;
+           }
+	}
+	
+	static int judgement (int user, int com)
+	{
+		if(user == 1) //주먹일 경우
+		{
+			if(com == 2) //주먹 vs 가위 > user 공격
+				return 1;
+			else if(com ==3) //주먹 vs 보 > com 공격
+				return 0;
+			else
+				return 0;
+		}
+		else if (user == 2)  //가위일 경우
+		{
+			if (com == 3) //가위 vs 보 > user 공격
+				return 1;
+			else if(com == 1) // 가위 vs 주먹 > com 공격
+				return 0;
+			else
+				return 0;
+		}
+		else if (user == 3) //보일 경우
+		{
+			if (com == 1) //보 vs 주먹 > user 공격
+				return 1;
+			else if(com == 2) //보 vs 가위 > com 공격
+				return 0;
+			else
+				return 0;
+		}
+			else
+				return 0;
+		}
+	
+	
     public static void main(String args[]){
-    
-        //프로그램 작동용 변수 및 배열 생성
-        String rps[] = { "가위", "바위", "보" };     //가위,바위, 보 비교용 배열 생성
+    	
+    	int user = 0; //user 상태 기록 변수
+    	int com = 0; //com 상태 기록 변수
+    	int judge = 0; //판정 상태 기록 변수
+    	int result = 0; //진행 상태 가록 변수
+
+        
         Scanner readin = new Scanner(System.in);    //input용 scanner 생성
-        int count = 0;                              //실행 횟수 확인용 count변수 생성
-        boolean run = true;                         //실행 여부 확인용 boolean타입 변수 생성
+        
+        
         
         //게임 동작
-            while(run){   //run이 true일때만 활성화
-                System.put.println("묵찌빠를 시작합니다.");
-                System.put.println("가위, 바위, 보 중에 선택해주세요.");
-        
-                String input = readin.nextLine();       // input문자열 변수에 입력 값 등록
-        
-                //프로그램 종료 조건 : stop 입력
-                if(input.equals("stop")){
-                    System.out.println("프로그램 종료");
-                    run = false;
-                    break;
+        System.out.println("====묵찌빠 게임을 시작합니다.====");
+       
+            do{   //run이 true일때만 활성화
+            	 System.out.println("1.[묵] 2.[찌] 3.[빠] 중에 선택해주세요. 0.[종료] ");
+                 System.out.println("선택:");
+                user = readin.nextInt();       // 숫자값을 통한 user 선택
+                com = (int)(Math.round(Math.random()*2)+1);	   // com 랜덤난수 설정
+                
+                if(user<0 || 3<user) //유효하지 않은 값 입력
+                {
+                 System.out.println("잘못 입력했습니다. 다시입력해주세요.\n");
+                 continue;
                 }
+                display(user,com);
+                if(user != com) //판정이 결정되면 루프 이탈
+                	break;
+            }while(true); //판정이 날 때까지 반복
                 
-                int rpsindex = (int)(Math.random) * rps.length);  //Math.random()을 통해 난수로 컴퓨터의 묵찌빠 정보 생성
-                String Comrps = rps[rpsindex];                    //선택된 정보를 Comrps에 저장
+                judge = judgement(user,com); //승부권 결정 메소드 호출 
                 
-                // 사용자가 낸 묵찌빠 정보와 컴퓨터가낸 묵찌빠 정보를 비교 
-	        switch(input) {
-	        	case "가위" : 
-	               		if(Comrps == "가위") {
-	               			System.out.println("비겼습니다~~ 아쉽네요");
-	               		}else if(Comrps == "바위"){
-	               			System.out.println("졌어요 ㅠㅠ 다음엔 이길거에요");
-	               		}else if(Comrps == "보") {
-	               			System.out.println("이겼어요!!!!!");
-	               		}; 
-	               		break;
-	               	case "바위" : 
-	               		if(Comrps == "바위") {
-	               			System.out.println("비겼습니다~~ 아쉽네요");
-	               		}else if(Comrps == "보"){
-	               			System.out.println("졌어요 ㅠㅠ 다음엔 이길거에요");
-	               		}else if(Comrps == "가위") {
-	               			System.out.println("이겼어요!!!!!");
-	               		}; 
-	               		break;
-	               	case "보" : 
-	               		if(Comrps == "보") {
-	               			System.out.println("비겼습니다~~ 아쉽네요");
-	               		}else if(Comrps == "가위"){
-	               			System.out.println("졌어요 ㅠㅠ 다음엔 이길거에요");
-	               		}else if(Comrps == "바위") {
-	               			System.out.println("이겼어요!!!!!");
-	               		}; 
-	               		break;
-	            }//switch()
-            }//while()
+                do {
+                	switch(judge) { //판정에 따라
+                	case 1 : 
+                		System.out.println("user 공격!");  //이기는 중일 때
+                		break;
+                	case 0 :
+                		System.out.println("Com 공격!"); //지고 있는 중일 때
+                	}
+                	 System.out.println("1.[묵] 2.[찌] 3.[빠] 중에 선택해주세요.");
+                	 System.out.println("선택:");
+                	 
+                	 user = readin.nextInt();
+                	 com = (int)(Math.round(Math.random()*2)+1);
+                	 
+                	 display(user,com); //상태 출력
+                	 if(user != com)
+                		 judge = judgement(user,com);
+                	 //결과가 나지 않았을 경우 선택에 따른 판정 재판독 후 결정
+                	 else result = 1;
+                	 //결과가 났을 경우 바로 루프 종료
+                }while(result == 0);
+            
+                if(judge == 1)
+                	System.out.println("축하합니다 ! User 승리!");
+                else
+                	System.out.println("아쉽습니다.. Com 승리!");
+         
+    
+            }
     }
-}
